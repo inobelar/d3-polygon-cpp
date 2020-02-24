9bbe0cfc097934d30caf6b2875eaa3c0fd834efb
Yurii Nakonechnyi <inobelar@gmail.com>
2020-02-24 23:25:59 +0200

    All tests passed successfully. Hull now works

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # [d3-polygon](https://github.com/d3/d3-polygon)-cpp
 
-## :warning: WIP/Unfinished & Experimental
+## TODO
 
-## TODO: hull still unfinished
+- [ ] `CMake` instead `qmake`
+
+## Usage
+
+In your `*.pro` file add:
+```qmake
+include(<path/to>/d3-polygon-cpp/src/d3_polygon.pri)
+````
--- src/d3_polygon/hull.hpp
+++ src/d3_polygon/hull.hpp
@@ -12,7 +12,9 @@ namespace detail {
 
 template <typename PointT>
 bool lexicographicOrder(const PointT& a, const PointT& b) {
-    return a[0] - b[0] || a[1] - b[1];
+    // NOTE: in c++ original js version not works: "a[0] - b[0] || a[1] - b[1]"
+    // References: https://stackoverflow.com/a/46371357 & https://rosettacode.org/wiki/Convex_hull#Go
+    return (a[0] == b[0]) ? (a[1] < b[1]) : (a[0] < b[0]);
 }
 
 // Computes the upper convex hull per the monotone chain algorithm.
@@ -26,10 +28,17 @@ std::vector<std::size_t> computeUpperHullIndexes(const std::vector<PointT>& poin
 
     for (std::size_t i = 2; i < n; ++i) {
         while (size > 1 && cross(points[indexes[size - 2]], points[indexes[size - 1]], points[i]) <= 0) --size;
-        indexes[size++] = i; // FIX
+
+        // Next 5 lines of code - implementation of: `indexes[size++] = i;`
+        if(size >= indexes.size()) {
+            indexes.push_back(i);
+        } else {
+            indexes[size] = i;
+        }
+        size++;
     }
 
-    indexes.resize(size); // indexes.slice(0, size); // remove popped points
+    indexes.resize(size); // the same as: `indexes.slice(0, size)`; // remove popped points
     return indexes;
 }
 
@@ -59,29 +68,31 @@ struct IndexedPoint {
 
 template <typename PointT>
 std::vector<PointT> hull(const std::vector<PointT>& points) {
+    using IPoint = detail::IndexedPoint< double >; // TODO: instead of `double` must be type of .x or .y point fields, or something like decltype(PointT::operator<std::size_t>[]) :)
+
     const std::size_t n = points.size();
     if (n < 3) return {};
 
     std::size_t i;
-    auto sortedPoints  = std::vector<detail::IndexedPoint<double>>(n);
+    auto sortedPoints  = std::vector<IPoint>(n);
     auto flippedPoints = std::vector<PointT>(n);
 
     for (i = 0; i < n; ++i) sortedPoints[i] = {points[i][0], points[i][1], i};
-    std::sort(sortedPoints.begin(), sortedPoints.end(), detail::lexicographicOrder<detail::IndexedPoint<double>>);
+    std::sort(sortedPoints.begin(), sortedPoints.end(), detail::lexicographicOrder<IPoint>);
     for (i = 0; i < n; ++i) flippedPoints[i] = {sortedPoints[i][0], -sortedPoints[i][1]};
 
     const auto upperIndexes = detail::computeUpperHullIndexes(sortedPoints),
                lowerIndexes = detail::computeUpperHullIndexes(flippedPoints);
 
     // Construct the hull polygon, removing possible duplicate endpoints.
-    const bool skipLeft = lowerIndexes[0] == upperIndexes[0],
+    const bool skipLeft  = lowerIndexes[0] == upperIndexes[0],
                skipRight = lowerIndexes[lowerIndexes.size() - 1] == upperIndexes[upperIndexes.size() - 1];
     std::vector<PointT> hull;
 
     // Add upper hull in right-to-l order.
     // Then add lower hull in left-to-right order.
-    for (int i = (upperIndexes.size() - 1); i >= 0; --i) hull.push_back(points[sortedPoints[upperIndexes[i]][2]]);
-    for (int i = (int)skipLeft; i < ((int)lowerIndexes.size() - (int)skipRight); ++i) hull.push_back(points[sortedPoints[lowerIndexes[i]][2]]);
+    for (i = (upperIndexes.size() ); i-- > 0; ) hull.push_back(points[sortedPoints[upperIndexes[i]][2]]); // NOTE: original loop code: `for(i = upperIndexes.size() - 1; i >= 0; --i)` NOT WORKS IN C++ (produces SEGFAULT) - due to `std::size_t i` comparison & overflow. References: https://stackoverflow.com/a/57935979 & https://stackoverflow.com/a/3626468
+    for (i = skipLeft; i < (lowerIndexes.size() - skipRight); ++i) hull.push_back(points[sortedPoints[lowerIndexes[i]][2]]);
 
     return hull;
 }
